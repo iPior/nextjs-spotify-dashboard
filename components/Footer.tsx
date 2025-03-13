153ffead90baf985894afc19a5d0a963c84d5a59
Piotr Szaran <szaranpiotr@gmail.com>
2025-03-12 22:25:45 -0400

    added links to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,13 +7,13 @@ export default function Footer(){
   return (
     <footer className='lg:h-1/12 flex flex-row justify-center px-1 pt-2 pb-4 text-md md:text-lg'>
       <div className=" flex justify-center  items-center">
-          <a href='' target="_blank" className='mr-8 hover:text-accent hover:font-bold hover:cursor-pointer'>
+          <a href='https://github.com/iPior' target="_blank" className='mr-8 hover:text-accent hover:font-bold hover:cursor-pointer'>
             <FontAwesomeIcon icon={faGithub} className='mr-1 ' />
             My Github
           </a>
       </div>
       <div className="flex justify-center items-center hover:text-accent hover:font-bold hover:cursor-pointer text-pretty">
-        <a href='' target="_blank" className='ml-8'>
+        <a href='https://github.com/iPior/nextjs-spotify-dashboard' target="_blank" className='ml-8'>
           <FontAwesomeIcon icon={faCode} className='mr-1 font-bold' />
           Source Code
         </a>
